9223adb408575933a1626b6bcbbc33dcf901f631
katao <k.katasan@gmail.com>
2025-12-15 13:14:23 +0900

    kanazawa

--- convert.sh
+++ convert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for f in `ls *.JPG *.jpg`;do
+# png以外のデータを変換
+for f in `ls imgs/*.JPG imgs/*.jpg`;do
     target=`echo $f | sed 's/\.[^\.]*$//'`
     echo $target
     convert -resize 300x300 $f $target.png
